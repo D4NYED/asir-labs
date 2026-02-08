f5f4fda13a6d5d4088c8d656d75714461ea6a9f9
Daniel Espinosa <158766129+D4NYED@users.noreply.github.com>
2026-02-08 19:06:02 +0100

    feat: add sample employee data
    Seed the database with initial employee records.

---
+++ 02-bases-de-datos/employees-database-queries/seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO Empleados VALUES
+(1, 'Sergio', 'González', '2000-01-25', 1, 1.78),
+(2, 'Carmen', 'González', '1970-05-22', 5, 2.00),
+(3, 'Leoncio', 'Lorca', '2000-10-24', 4, 1.69),
+(4, 'Zola', 'Ramos', '1978-01-30', NULL, NULL),
+(5, 'José', 'Hierro', '1950-01-25', 0, 2.05),
+(6, 'Paloma', NULL, '2000-08-08', 1, 1.50);
