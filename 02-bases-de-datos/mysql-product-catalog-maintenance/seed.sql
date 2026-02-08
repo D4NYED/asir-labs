e887ca36da078555639a77ebfa80f8203549e9c5
Daniel Espinosa <158766129+D4NYED@users.noreply.github.com>
2026-02-08 19:18:29 +0100

    feat: add sample product data

---
+++ 02-bases-de-datos/mysql-product-catalog-maintenance/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO Productos (NombreP, Precio, CategoriaP) VALUES
+('Mesa de Madera', 300.00, 'Mesas'),
+('Silla Oficina', 120.00, 'Sillas'),
+('Armario Blanco', 450.00, 'Armarios'),
+('Estantería Metal', 200.00, 'Estanterías'),
+('Sofá 3 plazas', 800.00, 'Sofás');
